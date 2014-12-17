a529f7b67b29b7831bcd2a7ee89da8105213d0e7
is_zhoufeng <is_zhoufeng@163.com>
2014-12-18 03:23:30 +0800

    jdis with spring c3p0

--- live/live-service/src/test/java/com/zf/live/service/test/RedisTest.java
+++ live/live-service/src/test/java/com/zf/live/service/test/RedisTest.java
@@ -1,9 +1,18 @@
 package com.zf.live.service.test;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -15,16 +24,60 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(
-		{"classpath*:service-beans.xml"}		
-)
+	{"classpath*:service-beans.xml"}		
+	)
 public class RedisTest {
 
-	@Autowired
-	private RedisTemplate<String, String> redisService ;
+    @Autowired
+    private RedisTemplate<String, String> stringTemplate ;
+    
+    @Resource(name="redisTemplate")
+    private ListOperations<String, String> listOps ;
+
+    @Resource(name="redisTemplate")
+    private ValueOperations<String, String> valueOps ;
+    
+    @Test
+    public void testString(){
 	
-	@Test
-	public void testConnect(){
-		System.out.println(redisService);  
+	
+	String result = stringTemplate.execute(new RedisCallback<String>() {
+	    @Override
+	    public String doInRedis(RedisConnection connection)
+		    throws DataAccessException { 
+		connection.set(stringTemplate.getStringSerializer().serialize("name"), 
+			stringTemplate.getStringSerializer().serialize("is_zhoufeng")); 
+		return "ok";
+	    }
+	});
+	System.out.println("OK");
+    }
+    
+    @Test
+    public void testSet(){
+	long result = listOps.leftPush("list", "b");
+	System.out.println(result);
+    }
+
+    @Test
+    public void testGet(){
+	List<String> ls = listOps.range("list", 0, -1); 
+	if(ls != null){
+	    for (String string : ls) {
+		System.out.println(string);
+	    } 
 	}
-	
+    }
+    
+    @Test
+    public void testSetValueOpt(){
+	valueOps.set("fff", "is_zhoufengx"); 
+    }
+    
+    @Test
+    public void testGetValueOpt(){
+	System.out.println(valueOps.get("fff"));
+    }
+    
+    
 }
