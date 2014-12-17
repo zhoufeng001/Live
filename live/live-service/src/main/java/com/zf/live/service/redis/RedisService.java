789426ca091b710bff11149db6a1264ace015c4d
zf <zf@zf-PC>
2014-12-18 00:27:25 +0800

    redis spring

--- live/live-service/src/main/java/com/zf/live/service/redis/RedisService.java
+++ live/live-service/src/main/java/com/zf/live/service/redis/RedisService.java
@@ -1,10 +1,5 @@
 package com.zf.live.service.redis;
 
-import com.zf.live.client.exception.RedisException;
-
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * Redis操作基础服务类
@@ -14,32 +9,5 @@ import redis.clients.jedis.JedisPoolConfig;
  */
 public class RedisService {
 
-	private String host ;
-	private int port ;
-	private String password ;
-
-	private JedisPool jedisPool ;
-	
-	public RedisService(String host , int port , String password){
-		this.host = host ;
-		this.port = port ;
-		this.password = password ;
-		initJedisPool() ;
-	}
-
-	private void initJedisPool(){
-		JedisPoolConfig config = new JedisPoolConfig() ;
-		config.setMaxIdle(30);
-		config.setMinIdle(10); 
-		config.setMaxWaitMillis(300);
-		jedisPool = new JedisPool(config, host, port, 3000 , password ) ;
-	}
-	
-	public Jedis getConnection(){
-		if(jedisPool == null){
-			throw new RedisException("jdisPool not Initialization...");
-		}
-		return jedisPool.getResource() ;
-	}
 	
 }
--- live/live-service/src/test/java/com/zf/live/service/test/RedisTest.java
+++ live/live-service/src/test/java/com/zf/live/service/test/RedisTest.java
@@ -3,12 +3,9 @@ package com.zf.live.service.test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import redis.clients.jedis.Jedis;
-
-import com.zf.live.service.redis.RedisService;
 
 /**
  * 
@@ -23,19 +20,11 @@ import com.zf.live.service.redis.RedisService;
 public class RedisTest {
 
 	@Autowired
-	private RedisService redisService ;
+	private RedisTemplate<String, String> redisService ;
 	
 	@Test
 	public void testConnect(){
-		Jedis jedis =redisService.getConnection() ;
-		System.out.println(jedis); 
-	}
-	
-	@Test
-	public void testSet(){
-		Jedis jedis =redisService.getConnection() ;
-		jedis.set("sex", "man") ;
-		System.out.println("OK");  
+		System.out.println(redisService);  
 	}
 	
 }
