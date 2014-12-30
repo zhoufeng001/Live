aaeca481a6d12cd4aa345449529070bfe0131bbd
zf <zf@zf-PC>
2014-12-30 14:45:50 +0800

    f

---
+++ live/live-common/src/main/java/com/zf/live/common/util/DesSecureFactory.java
@@ -0,0 +1,100 @@
+package com.zf.live.common.util;
+
+import java.security.SecureRandom;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.Base64Utils;
+
+/**
+ * Des加密工厂
+ * @author is_zhoufeng@163.com , QQ:243970446
+ * 2014年12月26日 上午1:25:37
+ */
+public class DesSecureFactory {
+	
+	static final Logger log = LoggerFactory.getLogger(DesSecureFactory.class);
+
+
+	private final static String DES = "DES";
+
+	/**
+	 * 根据密钥key获取一个加密工具类
+	 * @param key 密钥
+	 * @return
+	 */
+	public static DesSecure newInstance(String key){
+		try {
+			DESKeySpec dks = new DESKeySpec(key.getBytes());
+			SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DES);
+			SecretKey securekey = keyFactory.generateSecret(dks);
+
+			SecureRandom sr = new SecureRandom();
+			Cipher encrypt = Cipher.getInstance(DES);
+			encrypt.init(Cipher.ENCRYPT_MODE, securekey, sr);
+
+			Cipher decrypt = Cipher.getInstance(DES);
+			// 用密钥初始化Cipher对象
+			decrypt.init(Cipher.DECRYPT_MODE, securekey, sr);
+
+			return new DesSecure(encrypt , decrypt);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		}
+		return null ;
+	}
+
+
+	public static class DesSecure{
+
+		private Cipher encrypt ;
+		private Cipher decrypt ;
+
+		public DesSecure(Cipher encrypt, Cipher decrypt) {
+			this.encrypt = encrypt;
+			this.decrypt = decrypt;
+		}
+
+		/**
+		 * 解密
+		 * @param source
+		 * @return
+		 */
+		public String decrypt(String source){
+			byte[] bytes = null;
+			try {
+				byte[] sourceBytes = Base64Utils.decodeFromString(source);
+				bytes = decrypt.doFinal(sourceBytes);
+			} catch (IllegalBlockSizeException | BadPaddingException e) {
+				log.error(e.getMessage());
+				return null ;
+			}
+			return new String(bytes); 
+		}
+
+		/**
+		 * 加密
+		 * @param source
+		 * @return
+		 */
+		public String encrypt(String source){
+			byte[] result = null;
+			try {
+				result = encrypt.doFinal(source.getBytes());
+				return Base64Utils.encodeToString(result);
+			} catch (IllegalBlockSizeException | BadPaddingException e) {
+				log.error(e.getMessage());
+			}
+			return null ;
+		}
+
+	}
+
+}
