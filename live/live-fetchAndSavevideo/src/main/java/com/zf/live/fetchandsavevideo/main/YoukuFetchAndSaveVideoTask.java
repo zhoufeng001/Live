42e8b5e914a83b81523ad4abf89dbd20d9cea69c
is_zhoufeng <is_zhoufeng@163.com>
2015-01-08 00:21:12 +0800

    fas

--- live/live-fetchAndSavevideo/src/main/java/com/zf/live/fetchandsavevideo/main/YoukuFetchAndSaveVideoTask.java
+++ live/live-fetchAndSavevideo/src/main/java/com/zf/live/fetchandsavevideo/main/YoukuFetchAndSaveVideoTask.java
@@ -1,19 +1,52 @@
 package com.zf.live.fetchandsavevideo.main;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.zf.live.client.video.FetchVideosAndSaveService;
 import com.zf.live.service.impl.video.youku.YoukuFetchVideoAndSaveServiceImpl;
 
 public class YoukuFetchAndSaveVideoTask {
+
+	private static final Logger log = LoggerFactory.getLogger(YoukuFetchAndSaveVideoTask.class);
 	
 	public static void main(String[] args) {
 		
+		if(args == null || args.length == 0){
+			log.error("参数不能为空，第一个参数为隔多少个小时执行一次！");
+			return ;
+		}
+		
+		Integer hour = Integer.valueOf((args[0]));
+
+		ScheduledExecutorService threadPool = Executors.newScheduledThreadPool(1);
+		
 		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		final FetchVideosAndSaveService youkuFAS = applicationContext.getBean(YoukuFetchVideoAndSaveServiceImpl.class);
 		
-		FetchVideosAndSaveService youkuFAS = applicationContext.getBean(YoukuFetchVideoAndSaveServiceImpl.class);
+		applicationContext = null ;
 		
-		youkuFAS.fetchAndSave();		
+		log.info("任务启动成功，每{}小时执行一次！" , hour);
+		threadPool.scheduleWithFixedDelay(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					youkuFAS.fetchAndSave();
+				} catch (Exception e) {
+					log.error("----------抓取优酷视频出错---------------");
+					log.error(e.getMessage()); 
+				}		
+			}
+		}, 0 , hour , TimeUnit.HOURS);
+
+		
+
 		
 	}
 
